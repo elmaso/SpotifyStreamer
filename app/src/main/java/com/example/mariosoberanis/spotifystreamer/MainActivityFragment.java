3d764290c2655934cf42962f3408c35c3e2c8c55
marioS <elmaso@gmail.com>
2015-06-29 12:05:28 -0700

    We build the basic layouts and array adaptor

--- app/src/main/java/com/example/mariosoberanis/spotifystreamer/MainActivityFragment.java
+++ app/src/main/java/com/example/mariosoberanis/spotifystreamer/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.example.mariosoberanis.spotifystreamer;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -12,12 +14,38 @@ import android.view.ViewGroup;
  */
 public class MainActivityFragment extends Fragment {
 
+    ArrayAdapter<String> resultsAdapter;
+
     public MainActivityFragment() {
+
+        }
+
+
+        @Override
+        public View onCreateView (LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState){
+
+            View view = inflater.inflate(R.layout.artistfragment_main, container, false);
+
+            resultsAdapter = new ArrayAdapter<>(getActivity(), R.layout.search_result_list_item,
+                    R.id.artist_search_list_item_name, getDummyArtists());
+
+            ListView listView = (ListView) view.findViewById(R.id.artist_search_results_list);
+            listView.setAdapter(resultsAdapter);
+
+            return view;
+
+        }
+        private String[] getDummyArtists (){
+            return new String[]{
+                    "Muse",
+                    "Train",
+                    "OMD",
+                    "Mettalica",
+                    "Iggy Azalea",
+                    "Cricri",
+            };
+        }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.artistfragment_main, container, false);
-    }
-}
+
